0963182243030393e26e13bd5260a41416d6c260
teruru2017 <34904591+teruru2017@users.noreply.github.com>
2022-03-08 21:47:55 +0700

    8/3/22

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,9 +1,79 @@
-function Inventory() {
-    return <div className="container">
-        <div className="coming-soon-container">
-            <h1>COMING SOON</h1>
-        </div>
-    </div>
-}
+import React, { useState, useEffect } from "react";
+const App = () => { 
+    const [error, setErrors] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [airdrop, setAirdrop] = useState({})
+    useEffect(() => {
+      fetch("http://localhost:3333/airdrop/")
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setAirdrop(result);
+            console.log(result);
+          },
+          (error) => {
+            setAirdrop(true);
+            setErrors(error);
+          }
+        );
+    }, []);
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+    return(<tbody>
+       {airdrop.map((airdrop) => (
+      <h5>
+       {airdrop.accout}
+      </h5>
+      ))}
+      
+      </tbody>
+    }
+  }
+  
+//       <table>
+//         <thead>
+//           <tr>
+//             <th scope="col">#</th>
+//             <th scope="col">Name</th>
+//             <th scope="col">Username</th>
+//             <th scope="col">Avatar</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {airdrop.map((airdrop) => (
+//             <tr key={airdrop.id}>
+//               <th scope="row">{airdrop.id}</th>
+//               <td>
+//                 {airdrop.accout} {airdrop.boxid}
+//               </td>
+//               <td>{airdrop.boxtype}</td>
+//               <td>
+//                 <img
+//                   src={airdrop.img}
+//                   alt="avatar"
+//                   style={{ width: "30px" }}
+//                 />
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     );
+//   }
+// }
 
-export default Inventory;
+
+
+    // return <div className="container">
+    //    <h2>Airdrop</h2>
+      
+    //     <span>{JSON.stringify(airdrop)}</ span>
+    //     <hr />
+    //     <span>Has error : {JSON.stringify(hasError)}</span>
+       
+    // </div>
+export default App;
